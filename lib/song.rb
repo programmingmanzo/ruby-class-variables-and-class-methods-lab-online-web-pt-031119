3375ab6d04272b892f223dec32e63967ae795e6e
Gerardo Manzo <programmingmanzo@gmail.com>
2019-04-08 01:45:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,6 +29,13 @@ class Song
   
   def self.genre_count 
     genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1 
+      end
+    end
   end
   
   def self.artist
