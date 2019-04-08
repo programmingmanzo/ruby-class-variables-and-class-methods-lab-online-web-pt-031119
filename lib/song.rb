3850775b10667c0f2e3eae679c44c5cb344aafc5
Gerardo Manzo <programmingmanzo@gmail.com>
2019-04-08 02:11:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,5 +47,13 @@ class Song
   
   def self.artist_count 
     artist_count = {}
+    @@artist.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else 
+        artist_count[artist] = 1 
+      end 
+    end 
+    artist_count
   end
 end
